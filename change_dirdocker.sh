009689327287d5d32289b04780d34e2349a9e6b6
jarregui <jarregui@student.42madrid.com>
2024-04-16 10:26:13 +0200

    backup

---
+++ change_dirdocker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm -rf /Users/${USER}/Library/Containers/com.docker.docker && mkdir /sgoinfre/students/${USER}/com.docker.docker && ln -s /sgoinfre/students/${USER}/com.docker.docker /Users/${USER}/Library/Containers/com.docker.docker
+
